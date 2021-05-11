30bdba78a853f327f1608ff0272e47b55e698dc0
Yuyao-Xie <42394045+Yuyao-Xie@users.noreply.github.com>
2021-05-11 11:15:27 +0900

    delete unused method

--- Jupyter_Notebooks/PolynomialRegressionModel.py
+++ Jupyter_Notebooks/PolynomialRegressionModel.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
@@ -21,18 +20,3 @@ class PolynomialRegressionModel:
         x_poly = polynomial_features.fit_transform(x)
 
         return np.round(self.__model.predict(x_poly), 0).astype(np.int32)
-
-    def get_model_polynomial_str(self):
-        coef = self.__model.coef_
-        intercept = self.__model.intercept_
-        poly = "{0:.3f}".format(intercept)
-
-        for i in range(1, len(coef)):
-            if coef[i] >= 0:
-                poly += " + "
-            else:
-                poly += " - "
-            
-            poly += "{0:.3f}".format(coef[i]).replace("-", "") + "X^" + str(i)
-
-        return poly
